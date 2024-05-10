911addfefd5d538b16c5171d936f927d2bdcfee7
AlexCastro <castroecija.alejandro@gmail.com>
2024-05-10 17:34:23 +0200

    Added all funcionalities of Cart

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ for (let i = 0; i < productos.length; i++)
     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
       ${product.name} 
     </th>
-    <td class="px-6 py-4 text-right">
+    <td class="px-6 py-4">
 
     <button 
       name="add-item" 
@@ -146,7 +146,7 @@ for (let i = 0; i < productos.length; i++)
     </button>
 
     <button 
-      name="rmeove-item" 
+      name="remove-item" 
       id="edit-button-${i}" 
       type="button" 
       class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
@@ -187,18 +187,27 @@ function handleEditClick(event)
   
   switch (button.name) {
     case 'add-item':
-      let increased_stock = stock + 1;
+      let increased_stock = stock - 1;
       product.stock = increased_stock; 
       elemento.innerHTML = product.stock;
-      cart.addToCart(product);
+      if(product.stock !== 0)
+      {
+        cart.addToCart(product);
+      }
+      else{
+        document.getElementById(button.id).disabled = true;
+      }
+     
       console.log(cart); 
       break;  
         
     case 'remove-item':
-      let decrease_stock = stock - 1;
+      let decrease_stock = stock + 1;
       product.stock = decrease_stock;
       elemento.innerHTML = product.stock
       cart.removeFromCart(product.id);
+      //cart.updateCartSubtotal();
+      document.getElementById(button.id).disabled = false;
     break; 
 
     default:
--- models/CartProducts.js
+++ models/CartProducts.js
@@ -1,9 +1,10 @@
 import Product from "./Product.js";
 
 export default class CartProducts {
-    constructor(products = [], quantity = 0) {
+    constructor(products = [], quantity = 0, sub_total) {
         this.products = products;
         this.quantity = quantity;
+        this.sub_total = sub_total;
     }
 
     setProducts(products) { 
@@ -21,18 +22,26 @@ export default class CartProducts {
 
     addToCart(product) {
         var tbody_cart = document.querySelector('#tbody_cart');
+        
 
         const index = this.products.findIndex(p => p.id === product.id);
         console.log(index);
         if (index !== -1)
         {
             this.products[index].quantity++;
+            this.products[index].total = this.products[index].quantity * this.products[index].price;
             const elemento = document.getElementById(`uds_${product.id}`);
+            const total = document.getElementById(`total_${product.id}`);
             elemento.innerHTML = product.quantity;
+            total.innerHTML = product.total;
+
+            this.updateCartSubtotal();
+
         } 
         else 
         {
             product.quantity = 1;
+            //this.products[index].total = this.products[index].quantity * this.products[index].price +`€`;
             this.products.push(product);
 
             const row = `<tr id="item_${product.id}" class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
@@ -40,15 +49,16 @@ export default class CartProducts {
                 <td class="px-6 py-4">${product.category}</td>
                 <td class="px-6 py-4">${product.station}</td>
                 <td class="px-6 py-4">${product.price}</td> 
-                <td id="disponibilidad_${this.products.length - 1}" class="px-6 py-4">${product.stock}</td>
                 <td id="uds_${product.id}"class="px-6 py-4">${product.quantity}</td>
-                <td class="px-6 py-4">${product.stock}</td> 
+                <td id="total_${product.id}"class="px-6 py-4">${product.total = product.quantity * product.price}
+            </td>
             </tr>`;
-
+            this.updateCartSubtotal();
             tbody_cart.innerHTML += row;
         }
         // Increment products added quantity
         this.quantity++;
+         
     }
 
     removeFromCart(productId) 
@@ -58,27 +68,52 @@ export default class CartProducts {
         if (index !== -1) {
             this.products[index].quantity--;
             this.quantity--;
+            this.products[index].total = this.products[index].quantity * this.products[index].price;
+            this.products[index].total -= this.products[index].price;
             const uds = document.getElementById(`uds_${productId}`);
-
+            //console.log(sub_total);
             if (this.products[index].quantity === 0)
             {
                 this.products.splice(index, 1);
                 const row = document.querySelector(`#item_${productId}`);
-                row.remove();
+                row.remove();      
             }
             else    
-            {
+            {   
+                const sub_total = document.getElementById(`total_${productId}`);
                 uds.innerHTML = this.products[index].quantity;
+                sub_total.innerHTML = this.products[index].total;
+                
             }
+
+            
         }
+
+        this.updateCartSubtotal();
     }
 
-    // AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII VA BIEN ! mejor
+    // AHEAERWEDCWECWERV24RG ->
 
     removeCart(CartProducts) 
     {
         this.products = CartProducts = [];
-        var tbody_cart = document.querySelector('#tbody_cart');
+        var tbody_cart = document.getElementById(`tbody_cart`);
         tbody_cart.innerHTML = '';
+        const cart_total = document.getElementById(`total_cart`);
+        cart_total.innerHTML = '';
+    }
+
+    updateCartSubtotal()
+    {
+        this.sub_total = 0;
+        const cart_total = document.getElementById(`total_cart`);
+
+        for(let i = 0 ; i < this.products.length ; i++)
+        {
+            this.sub_total += this.products[i].total;
+            console.log(this.sub_total);
+        }
+        
+        cart_total.innerHTML = this.sub_total+`€`;
     }
 }
